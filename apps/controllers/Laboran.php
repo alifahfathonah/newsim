a48f03f0dc050fb8f228148ec34df25d4aec5021
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-04-02 23:11:36 +0700

    upload jadwal csv

--- apps/controllers/Laboran.php
+++ apps/controllers/Laboran.php
@@ -664,4 +664,81 @@ class Laboran extends CI_Controller
       redirect('Laboran/LaboratoryAssistant');
     }
   }
+
+  public function uploadJadwalCSV()
+  {
+    view('Laboran/csv');
+  }
+
+  public function simpanJadwalCSV()
+  {
+    $file = $_FILES['file_csv']['tmp_name'];
+    $ekstensi_file  = explode('.', $_FILES['file_csv']['name']);
+    if (strtolower(end($ekstensi_file)) === 'csv' && $_FILES['file_csv']['size'] > 0) {
+      $handle = fopen($file, 'r');
+      $i = 0;
+      while (($row = fgetcsv($handle, 2048))) {
+        $i++;
+        if ($i == 1) {
+          continue;
+        }
+        if ($row[0] == 'SENIN') {
+          $tanggal  = '2016-08-22';
+          $hari     = 1;
+        } elseif ($row[0] == 'SELASA') {
+          $tanggal = '2016-08-23';
+          $hari     = 2;
+        } elseif ($row[0] == 'RABU') {
+          $tanggal = '2016-08-24';
+          $hari     = 3;
+        } elseif ($row[0] == 'KAMIS') {
+          $tanggal = '2016-08-25';
+          $hari     = 4;
+        } elseif ($row[0] == 'JUMAT') {
+          $tanggal = '2016-08-26';
+          $hari     = 5;
+        } elseif ($row[0] == 'SABTU') {
+          $tanggal = '2016-08-27';
+          $hari     = 6;
+        }
+        $shift        = explode(' - ', $row[1]);
+        $jam_masuk    = $shift[0];
+        $jam_selesai  = $shift[1];
+        //
+        $matakuliah = $this->db->get('matakuliah')->result();
+        foreach ($matakuliah as $m) {
+          if ($m->kode_mk == $row[3]) {
+            $id_mk  = $m->id_mk;
+          }
+        }
+        $lab  = $this->db->get('laboratorium')->result();
+        foreach ($lab as $l) {
+          if ($l->kodeRuang == $row[2]) {
+            $id_lab = $l->idLab;
+          }
+        }
+        $kode_prodi = substr($row[4], 2, 2);
+        $prodi      = $this->db->get('prodi')->result();
+        foreach ($prodi as $p) {
+          if ($p->kode_prodi == $kode_prodi) {
+            $id_prodi = $p->id_prodi;
+          }
+        }
+        //
+        $input = array(
+          'jam_masuk'   => $tanggal . ' ' . $jam_masuk,
+          'jam_selesai' => $tanggal . ' ' . $jam_selesai,
+          'kelas'       => $row[4],
+          'kode_dosen'  => $row[5],
+          'hari_ke'     => $hari,
+          'id_mk'       => $id_mk,
+          'id_lab'      => $id_lab,
+          'id_prodi'    => $id_prodi
+        );
+        $this->db->insert('jadwal_lab', $input);
+        echo '<hr>';
+      }
+      fclose($handle);
+    }
+  }
 }
