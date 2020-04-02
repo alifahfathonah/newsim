ec3c782d67bf5f857567555b91234d443ac4b3e8
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-04-03 00:33:53 +0700

    set default timezone to Asia/Jakarta

--- apps/config/config.php
+++ apps/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-date_default_timezone_set("Asia/Bangkok");
+date_default_timezone_set("Asia/Jakarta");
 
 /*
 |--------------------------------------------------------------------------
