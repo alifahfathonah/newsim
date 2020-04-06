a0e1baad43f6dea8f3361ff4ecf85b4f0d4afe8c
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-04-07 03:33:08 +0700

    remove fixed-sidebar for laboran

--- apps/views/laboran/header.php
+++ apps/views/laboran/header.php
@@ -32,7 +32,7 @@
   </style>
 </head>
 
-<body class="fixed-sidebar">
+<body>
   <div id="wrapper">
     <nav class="navbar-default navbar-static-side" role="navigation">
       <div class="sidebar-collapse">
