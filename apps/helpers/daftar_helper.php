596604013e6f5c6ce70e8036efcdd119849ca47e
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-04-13 02:20:34 +0700

    add helper day name

--- apps/helpers/daftar_helper.php
+++ apps/helpers/daftar_helper.php
@@ -172,3 +172,29 @@ if (!function_exists('bulanPanjang')) {
     }
   }
 }
+
+if (!function_exists('hariInggris')) {
+  function hariInggris($hari)
+  {
+    switch ($hari) {
+      case 1:
+        return 'Monday';
+        break;
+      case 2:
+        return 'Tuesday';
+        break;
+      case 3:
+        return 'Wednesday';
+        break;
+      case 4:
+        return 'Thursday';
+        break;
+      case 5:
+        return 'Friday';
+        break;
+      case 6:
+        return 'Saturday';
+        break;
+    }
+  }
+}
