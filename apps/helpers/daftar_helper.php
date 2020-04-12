5285ff178b946fdb8d25e6e74288d37022432bd0
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-04-13 04:15:10 +0700

    add helper date format indonesian version

--- apps/helpers/daftar_helper.php
+++ apps/helpers/daftar_helper.php
@@ -198,3 +198,59 @@ if (!function_exists('hariInggris')) {
     }
   }
 }
+
+if (!function_exists('tanggal_indonesia')) {
+  function tanggal_indonesia($tanggal)
+  {
+    $ubahTanggal = gmdate($tanggal, time() + 60 * 60 * 8);
+    $pecahTanggal = explode('-', $ubahTanggal);
+    $tanggal = $pecahTanggal[2];
+    $bulan = bulan_panjang($pecahTanggal[1]);
+    $tahun = $pecahTanggal[0];
+    return $tanggal . ' ' . $bulan . ' ' . $tahun;
+  }
+}
+
+if (!function_exists('bulan_panjang')) {
+  function bulan_panjang($bulan)
+  {
+    switch ($bulan) {
+      case 1:
+        return 'Januari';
+        break;
+      case 2:
+        return 'Februari';
+        break;
+      case 3:
+        return 'Maret';
+        break;
+      case 4:
+        return 'April';
+        break;
+      case 5:
+        return 'Mei';
+        break;
+      case 6:
+        return 'Juni';
+        break;
+      case 7:
+        return 'Juli';
+        break;
+      case 8:
+        return 'Agustus';
+        break;
+      case 9:
+        return 'September';
+        break;
+      case 10:
+        return 'Oktober';
+        break;
+      case 11:
+        return 'November';
+        break;
+      case 12:
+        return 'Desember';
+        break;
+    }
+  }
+}
