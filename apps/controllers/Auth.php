6289062a6047828bec0a625383f9865e4b04c831
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-04-16 19:03:52 +0700

    change email host

--- apps/controllers/Auth.php
+++ apps/controllers/Auth.php
@@ -333,13 +333,13 @@ class Auth extends CI_Controller
     $response = false;
     $mail             = new PHPMailer();
     $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
+    $mail->Host       = 'ssl://smtp.gmail.com';
     $mail->SMTPAuth   = true;
     $mail->Username   = 'simlabfit@gmail.com';
     $mail->Password   = 'superlab5f1t';
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->From('simlabfit@gmail.com', 'SIM Laboratorium FIT');
+    $mail->setFrom('simlabfit@gmail.com', 'SIM Laboratorium FIT');
     $mail->addReplyTo('simlabfit@gmail.com', 'SIM Laboratorium FIT');
     $mail->addAddress($email);
     $mail->Subject    = 'Reset Your SIM Laboratorium Password';
@@ -350,6 +350,7 @@ class Auth extends CI_Controller
     $isi = view('auth/email_reset_password', $data, true);
     $mailContent  = $isi;
     $mail->Body = $mailContent;
+    echo !extension_loaded('openssl') ? "Not Available" : "Available";
     if (!$mail->send()) {
       echo 'Message could not be sent.<br>';
       echo 'Mailer Error: ' . $mail->ErrorInfo;
