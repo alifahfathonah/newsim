0662b43bf8ab21ed6d3ab90fde973a7fef469774
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-04-17 22:23:07 +0700

    add form & fix sleect option in month

--- apps/views/asprak/bap.php
+++ apps/views/asprak/bap.php
@@ -11,40 +11,42 @@
               echo '<div class="alert alert-danger">Please complete your personal information in <b>Setting Menu before submit BAP</b></div>';
             } else {
             ?>
-              <div class="row">
-                <div class="col-md-5 offset-md-1 col-sm-5" style="margin-bottom: 5px">
-                  <select name="matapraktikum" id="matapraktikum" class="matapraktikum form-control">
-                    <option></option>
-                    <?php
-                    foreach ($mk as $mk) {
-                    ?>
-                      <option value="<?= $mk->id_mk ?>"><?= $mk->strata . '' . $mk->kode_prodi . ' | ' . $mk->kode_mk . ' ' . $mk->nama_mk ?></option>
-                    <?php
-                    }
-                    ?>
-                  </select>
+              <form method="post" action="<?= base_url('Asprak/BAP') ?>">
+                <div class="row">
+                  <div class="col-md-5 offset-md-1 col-sm-5" style="margin-bottom: 5px">
+                    <select name="matapraktikum" id="matapraktikum" class="matapraktikum form-control">
+                      <option></option>
+                      <?php
+                      foreach ($mk as $mk) {
+                      ?>
+                        <option value="<?= $mk->id_daftar_mk ?>"><?= $mk->strata . '' . $mk->kode_prodi . ' | ' . $mk->kode_mk . ' ' . $mk->nama_mk ?></option>
+                      <?php
+                      }
+                      ?>
+                    </select>
+                  </div>
+                  <div class="col-md-3 col-sm-3" style="margin-bottom: 5px">
+                    <select name="bulan" id="bulan" class="periode_bap form-control">
+                      <option></option>
+                      <option value="'<?= date('Y') ?>-01-01' and '<?= date('Y') ?>-01-20'|1|Januari">January</option>
+                      <option value="'<?= date('Y') ?>-01-21' and '<?= date('Y') ?>-02-20'|2|Februari">February</option>
+                      <option value="'<?= date('Y') ?>-02-21' and '<?= date('Y') ?>-03-20'|3|Maret">March</option>
+                      <option value="'<?= date('Y') ?>-03-21' and '<?= date('Y') ?>-04-20'|4|April">April</option>
+                      <option value="'<?= date('Y') ?>-04-21' and '<?= date('Y') ?>-05-20'|5|Mei">May</option>
+                      <option value="'<?= date('Y') ?>-05-21' and '<?= date('Y') ?>-06-20'|6|Juni">June</option>
+                      <option value="'<?= date('Y') ?>-06-21' and '<?= date('Y') ?>-07-20'|7|Juli">July</option>
+                      <option value="'<?= date('Y') ?>-07-21' and '<?= date('Y') ?>-08-20'|8|Agustus">August</option>
+                      <option value="'<?= date('Y') ?>-08-21' and '<?= date('Y') ?>-09-20'|9|September">September</option>
+                      <option value="'<?= date('Y') ?>-09-21' and '<?= date('Y') ?>-10-20'|10|Oktober">October</option>
+                      <option value="'<?= date('Y') ?>-10-21' and '<?= date('Y') ?>-11-20'|11|November">November</option>
+                      <option value="'<?= date('Y') ?>-11-21' and '<?= date('Y') ?>-12-20'|12|Desember">December</option>
+                    </select>
+                  </div>
+                  <div class="col-md-3 col-sm-3">
+                    <button class="btn btn-primary btn-sm"><i class="fa fa-print"></i> Submit BAP</button>
+                  </div>
                 </div>
-                <div class="col-md-3 col-sm-3" style="margin-bottom: 5px">
-                  <select name="bulan" id="bulan" class="periode_bap form-control">
-                    <option></option>
-                    <option value="'<?= date('Y') ?>-01-01' and '<?= date('Y') ?>-01-20'|1">January</option>
-                    <option value="'<?= date('Y') ?>-01-21' and '<?= date('Y') ?>-02-20'|2">February</option>
-                    <option value="'<?= date('Y') ?>-02-21' and '<?= date('Y') ?>-03-20'|3">March</option>
-                    <option value="'<?= date('Y') ?>-03-21' and '<?= date('Y') ?>-04-20'|4">April</option>
-                    <option value="'<?= date('Y') ?>-04-21' and '<?= date('Y') ?>-05-20'|5">May</option>
-                    <option value="'<?= date('Y') ?>-05-21' and '<?= date('Y') ?>-06-20'|6">June</option>
-                    <option value="'<?= date('Y') ?>-06-21' and '<?= date('Y') ?>-07-20'|7">July</option>
-                    <option value="'<?= date('Y') ?>-07-21' and '<?= date('Y') ?>-08-20'|8">August</option>
-                    <option value="'<?= date('Y') ?>-08-21' and '<?= date('Y') ?>-09-20'|9">September</option>
-                    <option value="'<?= date('Y') ?>-09-21' and '<?= date('Y') ?>-10-20'|10">October</option>
-                    <option value="'<?= date('Y') ?>-10-21' and '<?= date('Y') ?>-11-20'|11">November</option>
-                    <option value="'<?= date('Y') ?>-11-21' and '<?= date('Y') ?>-12-20'|12">December</option>
-                  </select>
-                </div>
-                <div class="col-md-3 col-sm-3">
-                  <button class="btn btn-primary btn-sm"><i class="fa fa-print"></i> Submit BAP</button>
-                </div>
-              </div>
+              </form>
               <div class="ibox">
                 <input type="text" name="course" id="course" class="form-control" style="display: none;"><input type="text" name="month" id="month" class="form-control" style="display: none;">
                 <div class="ibox-content">
