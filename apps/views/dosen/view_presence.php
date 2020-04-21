497d245e77ebe38006afeb950fd8f0fd54fd7a34
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-04-22 02:55:19 +0700

    add view presence

---
+++ apps/views/dosen/view_presence.php
@@ -0,0 +1,69 @@
+      <div class="row wrapper border-bottom white-bg page-heading">
+        <div class="col-lg-12">
+          <h2 style="text-align: center">
+            View Presence for <?= $asprak->nama_asprak ?><br>
+            <?= $prodi->kode_mk . ' / ' . $prodi->nama_mk ?>
+          </h2>
+        </div>
+      </div>
+      <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+          <div class="col-md-12 col-sm-12">
+            <div class="ibox">
+              <div class="ibox-content">
+                <div class="table-responsive">
+                  <table class="table table-bordered" width="100%">
+                    <thead>
+                      <tr>
+                        <th width="5%">No</th>
+                        <th width="15%">Date</th>
+                        <th width="5%">Start</th>
+                        <th width="5%">End</th>
+                        <th width="20%">Modul</th>
+                        <th width="15%">Image</th>
+                        <th width="15%">Approve/Not</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php
+                      $no = 1;
+                      foreach ($bap as $b) {
+                        $image = '<a href="' . base_url($b->screenshot) . '" data-toggle="lightbox">
+                          <img src="' . base_url($b->screenshot) . '" height="60x">
+                        </a>';
+                      ?>
+                        <tr>
+                          <td><?= $no++ ?></td>
+                          <td><?= tanggal_inggris2($b->tanggal) ?></td>
+                          <td><?= $b->masuk ?></td>
+                          <td><?= $b->selesai ?></td>
+                          <td><?= $b->modul ?></td>
+                          <td style="text-align: center"><?= $image ?></td>
+                          <td style="text-align: center" class="align-middle" id="button<?= substr(sha1($b->id_presensi_asprak), 7, 7) ?>">
+                            <?php
+                            if ($b->approve_absen == '0') {
+                            ?>
+                              <button class="btn btn-success btn-sm" onclick="approve('<?= substr(sha1($b->id_presensi_asprak), 7, 7) ?>')"><i class="fa fa-check"></i> Approve</button>
+                              <button class="btn btn-danger btn-sm" onclick="pending('<?= substr(sha1($b->id_presensi_asprak), 7, 7) ?>')"><i class="fa fa-ban"></i> Pending</button>
+                            <?php
+                            } elseif ($b->approve_absen == '1') {
+                              echo '<button class="btn btn-success btn-sm" disabled><i class="fa fa-check"></i> Approved</button>';
+                            } elseif ($b->approve_absen == '2') {
+                              echo '<button class="btn btn-danger btn-sm" disabled><i class="fa fa-ban"></i> Pending</button>';
+                            }
+                            ?>
+                          </td>
+                        </tr>
+                      <?php
+                      }
+                      ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script>
+      </script>
