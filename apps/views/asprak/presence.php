9456b500ddea0c3fa0e017e6e6f4e736c0210217
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-04-21 23:01:25 +0700

    add button action for edit presence

--- apps/views/asprak/presence.php
+++ apps/views/asprak/presence.php
@@ -28,12 +28,13 @@
                     <thead>
                       <tr>
                         <th width="7%">No</th>
-                        <th width="25%">Date</th>
+                        <th width="20%">Date</th>
                         <th width="5%">Start</th>
                         <th width="5%">End</th>
                         <th>Courses</th>
                         <th width="10%">Class</th>
                         <th>Modul</th>
+                        <th width="7%">Action</th>
                       </tr>
                     </thead>
                     <tbody>
@@ -49,6 +50,23 @@
                           <td><?= $d->nama_mk ?></td>
                           <td><?= $d->kelas ?></td>
                           <td><?= $d->modul ?></td>
+                          <td style="text-align: center">
+                            <div class="tooltip-demo">
+                              <?php
+                              if ($d->approve_absen == '0') {
+                              ?>
+                                <a href="<?= base_url('Asprak/EditPresence/' . substr(sha1($d->id_presensi_asprak), 7, 7)) ?>">
+                                  <button class="btn btn-warning btn-sm" data-toggle="tooltip" data-placement="bottom" title="Edit your presence"><i class="fa fa-edit"></i></button>
+                                </a>
+                              <?php
+                              } elseif ($d->approve_absen == '1') {
+                              ?>
+                                <button class="btn btn-success btn-sm" data-toggle="tooltip" data-placement="bottom" title="Your presence approved by lecture"><i class="fa fa-check"></i></button>
+                              <?php
+                              }
+                              ?>
+                            </div>
+                          </td>
                         </tr>
                       <?php
                       }
