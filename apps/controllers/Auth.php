be391526d2cf4e3155dcdb1cff67945c44316382
Bayu Setya Ajie Perdana Putra <bayu.setya.ajie@gmail.com>
2020-04-23 00:11:21 +0700

    fix register for staff laboran

--- apps/controllers/Auth.php
+++ apps/controllers/Auth.php
@@ -267,18 +267,49 @@ class Auth extends CI_Controller
     } else {
       $nip_laboran    = input('nip_laboran');
       $username_user  = input('username_user');
+      $email_user     = input('email_user');
       $password_user  = sha1(input('password_user'));
-      $input          = array(
-        'username'    => $username_user,
-        'password'    => $password_user,
-        'id_laboran'  => $nip_laboran,
-        'jenisAkses'  => 'laboran',
-        'jabatan'     => 'Staff Laboratory',
-        'status'      => '1'
-      );
-      $this->auth->insertData('users', $input);
-      set_flashdata('msg', '<div class="alert alert-success msg">Thank you for register. Now you can login using your account.</div>');
-      redirect();
+      $cek_nip        = $this->db->get_where('laboran', array('nip_laboran' => $nip_laboran))->row();
+      $cek_akun       = $this->db->get_where('users', array('id_laboran' => $nip_laboran))->row();
+      if ($cek_nip) {
+        if ($cek_akun) {
+          set_flashdata('msg', '<div class="alert alert-danger msg">NIP already used to create account. You can login with your username and password.</div>');
+          redirect();
+        } else {
+          if (preg_match('/telkomuniversity.ac.id/', $email_user)) {
+            $input          = array(
+              'username'    => $username_user,
+              'password'    => $password_user,
+              'id_laboran'  => $nip_laboran,
+              'jenisAkses'  => 'laboran',
+              'jabatan'     => 'Staff Laboratory',
+              'status'      => '2'
+            );
+            $this->auth->insertData('users', $input);
+            $input  = array('email_laboran' => $email_user);
+            $this->db->where('nip_laboran', $nip_laboran)->update('laboran', $input);
+            $token  = base64_encode(random_bytes(32));
+            $this->email_konfirm_akun($cek_nip->nama_laboran, $email_user, $username_user, $token);
+            $waktu  = date('Y-m-d H:i:s');
+            $input  = array(
+              'email'             => $email_user,
+              'username'          => $username_user,
+              'nama_user'         => $cek_nip->nama_laboran,
+              'token'             => $token,
+              'tanggal_pengajuan' => $waktu
+            );
+            $this->auth->insertData('forgot_password', $input);
+            set_flashdata('msg', '<div class="alert alert-success">Thank you for register. Please check your inbox/spam to active your account</div>');
+            redirect();
+          } else {
+            set_flashdata('msg', '<div class="alert alert-danger">Sorry you must use email from Telkom University</div>');
+            redirect('Auth/RegisterStaff');
+          }
+        }
+      } else {
+        set_flashdata('msg', '<div class="alert alert-danger msg">Sorry your NIP not registered.</div>');
+        redirect('Auth/RegisterStaff');
+      }
     }
   }
 
